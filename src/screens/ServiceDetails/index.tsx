afeb47fe3d39c4311aff67a93e98cd1967a8dd97
Thiago Bignotto <bignotto@gmail.com>
2023-09-24 07:42:22 -0300

    save commit

--- src/screens/ServiceDetails/index.tsx
+++ src/screens/ServiceDetails/index.tsx
@@ -1,7 +1,10 @@
 import AppScreenContainer from "@components/AppScreenContainer";
 import AppText from "@components/AppText";
 import { useRoute } from "@react-navigation/native";
-import React from "react";
+import { api } from "@services/api";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { Service } from "src/@types/services/Service";
 
 type Params = {
   serviceId: string;
@@ -11,9 +14,36 @@ export default function ServiceDetails() {
   const route = useRoute();
   const { serviceId } = route.params as Params;
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [service, setService] = useState<Service>();
+
+  useEffect(() => {
+    async function loadService() {
+      try {
+        const response = await api.get(`/services/${serviceId}`);
+        setService(response.data);
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    loadService();
+  }, []);
+
   return (
     <AppScreenContainer>
-      <AppText>Service id {serviceId}</AppText>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <AppText size="lg" bold>
+            {service?.title}
+          </AppText>
+          <AppText>{service?.description}</AppText>
+        </>
+      )}
     </AppScreenContainer>
   );
 }
