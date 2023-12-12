2b839fe30863b41cdb85da125205c5e0680c9b3b
Thiago Bignotto <bignotto@gmail.com>
2023-12-12 07:03:57 -0300

    fixed undefined profile creation bug

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import AppScreenContainer from "@components/AppScreenContainer";
 import AppText from "@components/AppText";
+import Header from "@components/Header";
 import React from "react";
+import { useTheme } from "styled-components";
 
 export default function Dashboard() {
+  const theme = useTheme();
   return (
-    <AppScreenContainer>
+    <AppScreenContainer header={<Header />} headerColor={theme.colors.white}>
       <AppText size="xlg">Dashboard</AppText>
     </AppScreenContainer>
   );
