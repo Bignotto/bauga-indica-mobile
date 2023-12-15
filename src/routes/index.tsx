1f2432f9b27817109bb26646d6004c6b8c9f0ec9
Thiago Bignotto <bignotto@gmail.com>
2023-12-15 07:03:29 -0300

    save commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,83 +1,92 @@
+import { SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { Entypo, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Dashboard from "@screens/Dashboard";
 import Profile from "@screens/Profile/indext";
 import { useTheme } from "styled-components";
 import { PublicRoutes } from "./public.routes";
 
 const { Navigator, Screen } = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   const theme = useTheme();
+
   return (
     <NavigationContainer>
-      <Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Screen
-          name="Start"
-          component={PublicRoutes}
-          options={{
-            tabBarLabel: "Início",
-            tabBarActiveTintColor: theme.colors.primary,
-            tabBarLabelStyle: { fontSize: 14 },
-            tabBarIcon: (tabInfo) => (
-              <Entypo
-                name="home"
-                size={24}
-                color={
-                  tabInfo.focused
-                    ? theme.colors.primary
-                    : theme.colors.text_disabled
-                }
-              />
-            ),
+      <SignedOut>
+        <PublicRoutes />
+      </SignedOut>
+      <SignedIn>
+        <Navigator
+          screenOptions={{
+            headerShown: false,
           }}
-        />
-        <Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={{
-            tabBarLabel: "Dashboard",
-            tabBarActiveTintColor: theme.colors.primary,
-            tabBarLabelStyle: { fontSize: 14 },
-            tabBarIcon: (tabInfo) => (
-              <MaterialIcons
-                name="dashboard"
-                size={24}
-                color={
-                  tabInfo.focused
-                    ? theme.colors.primary
-                    : theme.colors.text_disabled
-                }
-              />
-            ),
-          }}
-        />
-        <Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarLabel: "Perfil",
-            tabBarActiveTintColor: theme.colors.primary,
-            tabBarLabelStyle: { fontSize: 14 },
-            tabBarIcon: (tabInfo) => (
-              <FontAwesome5
-                name="user-alt"
-                size={24}
-                color={
-                  tabInfo.focused
-                    ? theme.colors.primary
-                    : theme.colors.text_disabled
-                }
-              />
-            ),
-          }}
-        />
-      </Navigator>
+        >
+          <Screen
+            name="Start"
+            component={PublicRoutes}
+            options={{
+              tabBarLabel: "Início",
+              tabBarActiveTintColor: theme.colors.primary,
+              tabBarLabelStyle: { fontSize: 14 },
+              tabBarIcon: (tabInfo) => (
+                <Entypo
+                  name="home"
+                  size={24}
+                  color={
+                    tabInfo.focused
+                      ? theme.colors.primary
+                      : theme.colors.text_disabled
+                  }
+                />
+              ),
+            }}
+          />
+          <Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{
+              tabBarLabel: "Dashboard",
+              tabBarActiveTintColor: theme.colors.primary,
+              tabBarLabelStyle: { fontSize: 14 },
+              tabBarIcon: (tabInfo) => (
+                <MaterialIcons
+                  name="dashboard"
+                  size={24}
+                  color={
+                    tabInfo.focused
+                      ? theme.colors.primary
+                      : theme.colors.text_disabled
+                  }
+                />
+              ),
+            }}
+          />
+          <Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarLabel: "Perfil",
+              tabBarActiveTintColor: theme.colors.primary,
+              tabBarLabelStyle: { fontSize: 14 },
+              tabBarIcon: (tabInfo) => (
+                <FontAwesome5
+                  name="user-alt"
+                  size={24}
+                  color={
+                    tabInfo.focused
+                      ? theme.colors.primary
+                      : theme.colors.text_disabled
+                  }
+                />
+              ),
+            }}
+          />
+        </Navigator>
+      </SignedIn>
     </NavigationContainer>
   );
 }
