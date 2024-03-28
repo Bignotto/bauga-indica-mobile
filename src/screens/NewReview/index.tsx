b5f985f0a24dfcfb53f8b977cbda9c26b2ea8635
Thiago Bignotto <bignotto@gmail.com>
2024-03-28 06:12:24 -0300

    create review screen

--- src/screens/NewReview/index.tsx
+++ src/screens/NewReview/index.tsx
@@ -1,11 +1,14 @@
 import AppAvatar from "@components/AppAvatar";
+import AppButton from "@components/AppButton";
+import AppInput from "@components/AppInput";
 import AppScreenContainer from "@components/AppScreenContainer";
 import AppSpacer from "@components/AppSpacer";
 import AppText from "@components/AppText";
 import { FontAwesome } from "@expo/vector-icons";
 import { IUserServiceAd } from "@hooks/DataContext";
 import { useRoute } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
+import { RectButton } from "react-native-gesture-handler";
 import { ProviderInfoContainer, StarsContainer } from "./styles";
 
 type Params = {
@@ -15,6 +18,12 @@ type Params = {
 export default function NewReview() {
   const route = useRoute();
   const { service } = route.params as Params;
+
+  const [rate, setRate] = useState(0);
+
+  function handleClickRate(value: number) {
+    value === rate ? setRate(0) : setRate(value);
+  }
 
   return (
     <AppScreenContainer>
@@ -28,13 +37,56 @@ export default function NewReview() {
         <AppAvatar size={24} imagePath={`${service.providerId?.image}`} />
         <AppText>{service.providerId?.name}</AppText>
       </ProviderInfoContainer>
-
       <AppSpacer verticalSpace="xlg" />
       <AppText>Nota:</AppText>
       <StarsContainer>
-        <FontAwesome name="star" size={54} color="gold" />
-        <FontAwesome name="star-o" size={54} color="gold" />
+        <RectButton onPress={() => handleClickRate(1)}>
+          {rate >= 1 ? (
+            <FontAwesome name="star" size={54} color="gold" />
+          ) : (
+            <FontAwesome name="star-o" size={54} color="gold" />
+          )}
+        </RectButton>
+        <RectButton onPress={() => handleClickRate(2)}>
+          {rate >= 2 ? (
+            <FontAwesome name="star" size={54} color="gold" />
+          ) : (
+            <FontAwesome name="star-o" size={54} color="gold" />
+          )}
+        </RectButton>
+        <RectButton onPress={() => handleClickRate(3)}>
+          {rate >= 3 ? (
+            <FontAwesome name="star" size={54} color="gold" />
+          ) : (
+            <FontAwesome name="star-o" size={54} color="gold" />
+          )}
+        </RectButton>
+        <RectButton onPress={() => handleClickRate(4)}>
+          {rate >= 4 ? (
+            <FontAwesome name="star" size={54} color="gold" />
+          ) : (
+            <FontAwesome name="star-o" size={54} color="gold" />
+          )}
+        </RectButton>
+        <RectButton onPress={() => handleClickRate(5)}>
+          {rate >= 5 ? (
+            <FontAwesome name="star" size={54} color="gold" />
+          ) : (
+            <FontAwesome name="star-o" size={54} color="gold" />
+          )}
+        </RectButton>
       </StarsContainer>
+      <AppSpacer />
+      <AppInput label="Título:" />
+      <AppInput
+        label="Avaliação:"
+        multiline={true}
+        numberOfLines={4}
+        textAlignVertical="top"
+      />
+      <AppButton title="Enviar avaliação" variant="positive" />
+      <AppSpacer />
+      <AppButton title="Cancelar" variant="negative" outline />
     </AppScreenContainer>
   );
 }
