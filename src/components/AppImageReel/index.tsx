632a915c7066c7aef3287bf8efc384545e7fb2f5
Thiago Bignotto <bignotto@gmail.com>
2024-05-07 06:30:50 -0300

    :floppy_disk: feat: image reel index indicator

--- src/components/AppImageReel/index.tsx
+++ src/components/AppImageReel/index.tsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { Dimensions, Image, ListRenderItem } from "react-native";
+import AppText from "@components/AppText";
+import React, { useCallback, useState } from "react";
+import {
+  Dimensions,
+  Image,
+  ListRenderItem,
+  View,
+  ViewToken,
+} from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-
-interface AppImageReelProps {
-  images:
-    | {
-        id: number;
-        imagePath: string;
-      }[]
-    | undefined;
-}
+import { IndexIndicator } from "./styles";
 
 type ImageItem = {
   id: number;
   imagePath: string;
 };
 
+interface AppImageReelProps {
+  images: ImageItem[] | undefined;
+}
+
+type HandleScrollProps = {
+  viewableItems: Array<ViewToken>;
+};
+
 export default function AppImageReel({ images }: AppImageReelProps) {
+  const [viewIndex, setViewIndex] = useState(0);
+
   const renderItem: ListRenderItem<ImageItem> = ({ item }) => {
     return (
       <Image
@@ -30,19 +39,51 @@ export default function AppImageReel({ images }: AppImageReelProps) {
     );
   };
 
+  const onViewChangeCallback = useCallback(
+    ({ viewableItems }: HandleScrollProps) => {
+      setViewIndex(viewableItems[0].index ?? 0);
+      console.log(viewableItems[0].index ?? 0);
+    },
+    []
+  );
+
   //NEXT: implement image counter
 
+  if (!images) return <AppText>Algo errado com as imagens</AppText>;
+
+  const imageIndexWidth = (Dimensions.get("window").width - 4) / images.length;
+
   return (
-    <FlatList
-      horizontal
-      decelerationRate={"fast"}
-      snapToInterval={Dimensions.get("window").width - 16}
-      // onViewableItemsChanged={({ viewableItems }: HandleScrollProps) =>
-      //   console.log({ viewableItems })
-      // }
-      data={images}
-      keyExtractor={(item) => `${item.id}`}
-      renderItem={renderItem}
-    />
+    <View>
+      <FlatList
+        horizontal
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").width - 16}
+        onViewableItemsChanged={onViewChangeCallback}
+        // onViewableItemsChanged={({ viewableItems }: HandleScrollProps) =>
+        //   console.log({ viewableItems })
+        // }
+        data={images}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={renderItem}
+      />
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {
+          //NEXT: fix indicator position
+          images.map((img, i) => (
+            <IndexIndicator
+              key={img.id}
+              indexWidth={imageIndexWidth * i}
+              highlight={i === viewIndex}
+            />
+          ))
+        }
+      </View>
+    </View>
   );
 }
--- src/components/AppImageReel/styles.ts
+++ src/components/AppImageReel/styles.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components/native";
 
+interface IndexIndicatorProps {
+  indexWidth: number;
+  highlight: boolean;
+}
+
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const IndexIndicator = styled.View<IndexIndicatorProps>`
+  position: absolute;
+  left: ${({ theme, indexWidth }) => indexWidth}px;
+  bottom: 8px;
+  height: 1px;
+  width: ${({ theme, indexWidth }) => indexWidth - 10}px;
+  background-color: #1a202c;
+  opacity: ${({ theme, indexWidth, highlight }) => (highlight ? 0.8 : 0.5)};
 `;
